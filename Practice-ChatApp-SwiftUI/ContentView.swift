f761fba133f6f17ede1da9157dbd4b700413cbc6
Stanley Miller <smiller8203@gmail.com>
2022-01-31 12:38:55 -0500

    Initial Commit

---
+++ Practice-ChatApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice-ChatApp-SwiftUI
+//
+//  Created by Stanley Miller on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practice-ChatApp-SwiftUI/Practice_ChatApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_ChatApp_SwiftUIApp.swift
+//  Practice-ChatApp-SwiftUI
+//
+//  Created by Stanley Miller on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Practice_ChatApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
